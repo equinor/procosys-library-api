4de5088b48c3fdba5cb09d66f3cf4d57146a3ac2
Henning Bjørgo <HHBJ@equinor.com>
2020-03-27 13:10:23 +0100

    Reading settings from Azure Key Vault.

---
+++ src/Equinor.Procosys.Library.WebApi/Misc/KeyVaultSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Equinor.Procosys.Library.WebApi.Misc
+{
+    public class KeyVaultSettings
+    {
+        public bool Enabled { get; set; } = true;
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string Uri { get; set; }
+    }
+}
