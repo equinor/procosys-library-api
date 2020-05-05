2dded272d1aedbc1c57960b4887d2cc18661919d
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-05-05 09:33:00 +0200

    Get registers endpoint and handler

---
+++ src/Equinor.Procosys.Library.Query/GetAllRegisters/MainApiRegister.cs
@@ -0,0 +1,8 @@
+﻿namespace Equinor.Procosys.Library.Query.GetAllRegisters
+{
+    public class MainApiRegister
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/Equinor.Procosys.Library.Query/GetAllRegisters/RegisterDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Equinor.Procosys.Library.Query.GetAllRegisters
+{
+    public class RegisterDto
+    {
+        public RegisterDto(string code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public string Code { get; }
+        public string Description { get; }
+    }
+}
