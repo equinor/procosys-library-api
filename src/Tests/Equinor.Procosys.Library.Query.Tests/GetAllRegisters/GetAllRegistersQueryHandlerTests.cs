74666ae67387da559a5bea2d501939f12c149cdc
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-05-05 10:10:29 +0200

    unit tests

---
+++ src/Tests/Equinor.Procosys.Library.Query.Tests/GetAllRegisters/GetAllRegistersQueryHandlerTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.Procosys.Library.Query.Client;
+using Equinor.Procosys.Library.Query.GetAllRegisters;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ServiceResult;
+
+namespace Equinor.Procosys.Library.Query.Tests.GetAllRegisters
+{
+    [TestClass]
+    public class GetAllRegistersQueryHandlerTests
+    {
+        private const string Plant = "PCS$TESTPLANT";
+        private Mock<IBearerTokenApiClient> _clientMock;
+        private Mock<IOptionsMonitor<MainApiOptions>> _optionsMonitorMock;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new MainApiOptions
+            {
+                ApiVersion = "1",
+                Audience = "Aud",
+                BaseAddress = "http://example.com/"
+            };
+
+            _optionsMonitorMock = new Mock<IOptionsMonitor<MainApiOptions>>();
+            _optionsMonitorMock
+                .Setup(x => x.CurrentValue)
+                .Returns(options);
+
+            var url = $"{options.BaseAddress}Library/Registers" +
+                $"?plantId={Plant}" +
+                $"&api-version={options.ApiVersion}";
+
+           var registers = new List<MainApiRegister>
+                {
+                    new MainApiRegister
+                    {
+                        Code = "CodeA",
+                        Description = "DescriptionA"
+                    },
+                    new MainApiRegister
+                    {
+                        Code = "CodeB",
+                        Description = "DescriptionB"
+                    }
+                };
+
+            _clientMock = new Mock<IBearerTokenApiClient>();
+            _clientMock
+                .Setup(x => x.QueryAndDeserialize<List<MainApiRegister>>(url))
+                .Returns(Task.FromResult(registers));
+        }
+
+        [TestMethod]
+        public async Task Handle_ReturnsOkResult()
+        {
+            var dut = new GetAllRegistersQueryHandler(_clientMock.Object, _optionsMonitorMock.Object);
+            var request = new GetAllRegistersQuery(Plant);
+
+            var result = await dut.Handle(request, default);
+
+            Assert.AreEqual(ResultType.Ok, result.ResultType);
+        }
+
+        [TestMethod]
+        public async Task Handle_ReturnsCorrectNumberOfElements()
+        {
+            var dut = new GetAllRegistersQueryHandler(_clientMock.Object, _optionsMonitorMock.Object);
+            var request = new GetAllRegistersQuery(Plant);
+
+            var result = await dut.Handle(request, default);
+
+            Assert.AreEqual(2, result.Data.Count());
+        }
+
+        [TestMethod]
+        public async Task Handle_ProjectsElementsCorrectly()
+        {
+            var dut = new GetAllRegistersQueryHandler(_clientMock.Object, _optionsMonitorMock.Object);
+            var request = new GetAllRegistersQuery(Plant);
+
+            var result = await dut.Handle(request, default);
+
+            Assert.AreEqual("CodeA", result.Data.ElementAt(0).Code);
+            Assert.AreEqual("DescriptionA", result.Data.ElementAt(0).Description);
+            Assert.AreEqual("CodeB", result.Data.ElementAt(1).Code);
+            Assert.AreEqual("DescriptionB", result.Data.ElementAt(1).Description);
+        }
+
+        [TestMethod]
+        public async Task Handle_ReturnsEmptyList_IfNoElementsAreFound()
+        {
+            _clientMock
+                .Setup(x => x.QueryAndDeserialize<List<MainApiRegister>>(It.IsAny<string>()))
+                .Returns(Task.FromResult<List<MainApiRegister>>(null));
+
+            var dut = new GetAllRegistersQueryHandler(_clientMock.Object, _optionsMonitorMock.Object);
+            var request = new GetAllRegistersQuery(Plant);
+
+            var result = await dut.Handle(request, default);
+
+            Assert.AreEqual(0, result.Data.Count());
+        }
+    }
+}
---
+++ src/Tests/Equinor.Procosys.Library.Query.Tests/GetAllRegisters/GetAllRegistersQueryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Equinor.Procosys.Library.Query.GetAllRegisters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Library.Query.Tests.GetAllRegisters
+{
+    [TestClass]
+    public class GetAllRegistersQueryTests
+    {
+        [TestMethod]
+        public void Constructor_SetsProperties()
+        {
+            var dut = new GetAllRegistersQuery("PCS$TESTPLANT");
+
+            Assert.AreEqual("PCS$TESTPLANT", dut.Plant);
+        }
+
+        [TestMethod]
+        public void Constructor_ThrowsException_WhenNoPlantIsGiven() =>
+            Assert.ThrowsException<ArgumentNullException>(() => new GetAllRegistersQuery(null));
+    }
+}
