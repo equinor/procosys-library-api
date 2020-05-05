49353256fba5c1c7f31983cd642d32eeda688d78
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-05-05 15:30:59 +0200

    tests

---
+++ src/Tests/Equinor.Procosys.Library.Query.Tests/GetTagFunctions/GetTagFunctionsQueryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Equinor.Procosys.Library.Query.GetTagFunctions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Library.Query.Tests.GetTagFunctions
+{
+    [TestClass]
+    public class GetTagFunctionsQueryTests
+    {
+        [TestMethod]
+        public void Constructor_SetsProperties()
+        {
+            var dut = new GetTagFunctionsQuery("PCS$TESTPLANT", "A");
+
+            Assert.AreEqual("PCS$TESTPLANT", dut.Plant);
+            Assert.AreEqual("A", dut.RegisterCode);
+        }
+
+        [TestMethod]
+        public void Constructor_ThrowsException_WhenNoPlantIsGiven() =>
+            Assert.ThrowsException<ArgumentNullException>(() => new GetTagFunctionsQuery(null, "A"));
+    }
+}
