b22161726f9e05e91abbc9d453769ed5f3decc45
Lykke Larsen <norl@statoil.com>
2020-05-25 14:57:04 +0200

    tests

---
+++ src/Tests/Equinor.Procosys.Library.Query.Tests/GetAllResponsibles/GetAllResponsiblesQueryHandlerTests.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.Procosys.Library.Query.Client;
+using Equinor.Procosys.Library.Query.GetAllResponsibles;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ServiceResult;
+
+namespace Equinor.Procosys.Library.Query.Tests.GetAllResponsibles
+{
+    [TestClass]
+    public class GetAllResponsiblesQueryHandlerTests
+    {
+        private const string Plant = "PCS$TESTPLANT";
+        private Mock<IBearerTokenApiClient> _clientMock;
+        private Mock<IOptionsMonitor<MainApiOptions>> _optionsMonitorMock;
+        private GetAllResponsiblesQueryHandler _dut;
+        private GetAllResponsiblesQuery _request;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new MainApiOptions
+            {
+                ApiVersion = "1",
+                Audience = "Aud",
+                BaseAddress = "http://example.com/"
+            };
+
+            _optionsMonitorMock = new Mock<IOptionsMonitor<MainApiOptions>>();
+            _optionsMonitorMock
+                .Setup(x => x.CurrentValue)
+                .Returns(options);
+
+            _request = new GetAllResponsiblesQuery(Plant);
+            var url = $"{options.BaseAddress}Library/Responsibles" +
+                $"?plantId={_request.Plant}" +
+                $"&api-version={options.ApiVersion}";
+
+            var responsibles = new List<MainApiResponsible>
+                {
+                    new MainApiResponsible
+                    {
+                        Code = "CodeA",
+                        Description = "DescriptionA"
+                    },
+                    new MainApiResponsible
+                    {
+                        Code = "CodeB",
+                        Description = "DescriptionB"
+                    }
+                };
+
+            _clientMock = new Mock<IBearerTokenApiClient>();
+            _clientMock
+                .Setup(x => x.QueryAndDeserialize<List<MainApiResponsible>>(url))
+                .Returns(Task.FromResult(responsibles));
+            _dut = new GetAllResponsiblesQueryHandler(_clientMock.Object, _optionsMonitorMock.Object);
+        }
+
+        [TestMethod]
+        public async Task Handle_ReturnsOkResult()
+        {
+            var result = await _dut.Handle(_request, default);
+
+            Assert.AreEqual(ResultType.Ok, result.ResultType);
+        }
+
+        [TestMethod]
+        public async Task Handle_ReturnsCorrectNumberOfElements()
+        {
+            var result = await _dut.Handle(_request, default);
+
+            Assert.AreEqual(2, result.Data.Count());
+        }
+
+        [TestMethod]
+        public async Task Handle_ProjectsElementsCorrectly()
+        {
+            var result = await _dut.Handle(_request, default);
+
+            Assert.AreEqual("CodeA", result.Data.ElementAt(0).Code);
+            Assert.AreEqual("DescriptionA", result.Data.ElementAt(0).Description);
+            Assert.AreEqual("CodeB", result.Data.ElementAt(1).Code);
+            Assert.AreEqual("DescriptionB", result.Data.ElementAt(1).Description);
+        }
+
+        [TestMethod]
+        public async Task Handle_ReturnsEmptyList_IfNoElementsAreFound()
+        {
+            _clientMock
+                .Setup(x => x.QueryAndDeserialize<List<MainApiResponsible>>(It.IsAny<string>()))
+                .Returns(Task.FromResult<List<MainApiResponsible>>(null));
+
+            var dut = new GetAllResponsiblesQueryHandler(_clientMock.Object, _optionsMonitorMock.Object);
+
+            var result = await dut.Handle(_request, default);
+
+            Assert.AreEqual(0, result.Data.Count());
+        }
+    }
+}
---
+++ src/Tests/Equinor.Procosys.Library.WebApi.Tests/Controllers/Responsible/ResponsiblesControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Library.Query.GetAllResponsibles;
+using Equinor.Procosys.Library.WebApi.Controllers.Responsible;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ServiceResult;
+
+namespace Equinor.Procosys.Library.WebApi.Tests.Controllers
+{
+    [TestClass]
+    public class ResponsiblesControllerTests
+    {
+        private const string Plant = "PCS$TESTPLANT";
+        private Mock<IMediator> _mediatorMock;
+
+        [TestInitialize]
+        public void Setup() => _mediatorMock = new Mock<IMediator>();
+
+        [TestMethod]
+        public async Task GetAllResponsiblesAsync_Returns200Ok()
+        {
+            _mediatorMock
+                .Setup(x => x.Send(It.IsAny<GetAllResponsiblesQuery>(), It.IsAny<CancellationToken>()))
+                .Returns(
+                    Task.FromResult(
+                        (Result<IEnumerable<ResponsibleDto>>)new SuccessResult<IEnumerable<ResponsibleDto>>(
+                            new List<ResponsibleDto>
+                                {
+                                    new ResponsibleDto("CodeA", "DescriptionA"),
+                                    new ResponsibleDto("CodeB", "DescriptionB"),
+                                })));
+
+            var dut = new ResponsiblesController(_mediatorMock.Object);
+
+            var result = await dut.GetAllResponsiblesAsync(Plant);
+
+            Assert.AreEqual(StatusCodes.Status200OK, ((OkObjectResult)result.Result).StatusCode);
+        }
+
+        [TestMethod]
+        public async Task GetAllResponsiblesAsync_ReturnsArrayOfElements()
+        {
+            var responsibles = new List<ResponsibleDto>
+                {
+                    new ResponsibleDto("CodeA", "DescriptionA"),
+                    new ResponsibleDto("CodeB", "DescriptionB"),
+                };
+
+            _mediatorMock
+                .Setup(x => x.Send(It.IsAny<GetAllResponsiblesQuery>(), It.IsAny<CancellationToken>()))
+                .Returns(
+                    Task.FromResult(
+                        (Result<IEnumerable<ResponsibleDto>>)new SuccessResult<IEnumerable<ResponsibleDto>>(responsibles)));
+
+            var dut = new ResponsiblesController(_mediatorMock.Object);
+
+            var result = await dut.GetAllResponsiblesAsync(Plant);
+
+            var items = (IEnumerable<ResponsibleDto>)((OkObjectResult)result.Result).Value;
+            Assert.AreEqual(2, items.Count());
+            Assert.AreEqual(responsibles[0], items.ElementAt(0));
+            Assert.AreEqual(responsibles[1], items.ElementAt(1));
+        }
+    }
+}
