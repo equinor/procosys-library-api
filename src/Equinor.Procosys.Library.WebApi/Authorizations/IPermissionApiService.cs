4be700a5907cc0d116f5db7d81d72eb5e4a80ff5
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-08-12 08:49:20 +0200

    Added IPermissionApiService

---
+++ src/Equinor.Procosys.Library.WebApi/Authorizations/IPermissionApiService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Equinor.Procosys.Library.WebApi.Authorizations
+{
+    public interface IPermissionApiService
+    {
+        Task<IList<string>> GetPermissionsAsync(string plantId);
+    }
+}
---
+++ src/Equinor.Procosys.Library.WebApi/Authorizations/MainApiPermissionService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Equinor.Procosys.Library.Query.Client;
+using Microsoft.Extensions.Options;
+
+namespace Equinor.Procosys.Library.WebApi.Authorizations
+{
+    public class MainApiPermissionService : IPermissionApiService
+    {
+        private readonly string _apiVersion;
+        private readonly Uri _baseAddress;
+        private readonly IBearerTokenApiClient _mainApiClient;
+
+        public MainApiPermissionService(IBearerTokenApiClient mainApiClient,
+            IOptionsMonitor<MainApiOptions> options)
+        {
+            _mainApiClient = mainApiClient;
+            _apiVersion = options.CurrentValue.ApiVersion;
+            _baseAddress = new Uri(options.CurrentValue.BaseAddress);
+        }
+        public async Task<IList<string>> GetPermissionsAsync(string plantId)
+        {
+            var url = $"{_baseAddress}Permissions" +
+                      $"?plantId={plantId}" +
+                      $"&api-version={_apiVersion}";
+
+            return await _mainApiClient.QueryAndDeserializeAsync<List<string>>(url) ?? new List<string>();
+        }
+    }
+}
