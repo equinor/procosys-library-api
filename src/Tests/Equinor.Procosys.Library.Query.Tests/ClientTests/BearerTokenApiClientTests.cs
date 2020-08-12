5670931f6caadbdff5d116ab4f4556a5724ea9d3
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-08-12 09:04:06 +0200

    unit tests

--- src/Tests/Equinor.Procosys.Library.Query.Tests/ClientTests/BearerTokenApiClientTests.cs
+++ src/Tests/Equinor.Procosys.Library.Query.Tests/ClientTests/BearerTokenApiClientTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Equinor.Procosys.Library.Query.Client;
@@ -22,39 +23,58 @@ namespace Equinor.Procosys.Library.Query.Tests.ClientTests
         }
 
         [TestMethod]
-        public async Task QueryAndDeserialize_ReturnsDeserialized_Object_TestAsync()
+        public async Task QueryAndDeserialize_ReturnsDeserialized_Object()
         {
             var httpClientFactory = HttpHelper.GetHttpClientFactory(HttpStatusCode.OK, "{\"Id\": 123}");
             var dut = new BearerTokenApiClient(httpClientFactory, _bearerTokenProvider.Object, _logger.Object);
 
-            var response = await dut.QueryAndDeserializeAsync<DummyClass>("");
+            var response = await dut.QueryAndDeserializeAsync<DummyClass>("url");
 
             Assert.IsNotNull(response);
             Assert.AreEqual(123, response.Id);
         }
 
         [TestMethod]
-        public async Task QueryAndDeserializeReturnsDefaultObject_WhenRequestIsNotSuccessful_TestAsync()
+        public async Task QueryAndDeserializeReturnsDefaultObject_WhenRequestIsNotSuccessful()
         {
             var httpClientFactory = HttpHelper.GetHttpClientFactory(HttpStatusCode.BadGateway, "");
             var dut = new BearerTokenApiClient(httpClientFactory, _bearerTokenProvider.Object, _logger.Object);
 
-            var response = await dut.QueryAndDeserializeAsync<DummyClass>("");
+            var response = await dut.QueryAndDeserializeAsync<DummyClass>("url");
 
             Assert.AreEqual(default, response);
         }
 
         [TestMethod]
-        public async Task QueryAndDeserialize_ThrowsException_WhenInvalidResponseIsReceived_TestAsync()
+        public async Task QueryAndDeserialize_ThrowsException_WhenInvalidResponseIsReceived()
         {
             var httpClientFactory = HttpHelper.GetHttpClientFactory(HttpStatusCode.OK, "");
             var dut = new BearerTokenApiClient(httpClientFactory, _bearerTokenProvider.Object, _logger.Object);
 
-            await Assert.ThrowsExceptionAsync<JsonException>(async () => await dut.QueryAndDeserializeAsync<DummyClass>(""));
+            await Assert.ThrowsExceptionAsync<JsonException>(async () => await dut.QueryAndDeserializeAsync<DummyClass>("url"));
+        }
+
+        [TestMethod]
+        public async Task QueryAndDeserialize_ThrowsException_WhenNoUrl()
+        {
+            var httpClientFactory = HttpHelper.GetHttpClientFactory(HttpStatusCode.OK, "");
+            var dut = new BearerTokenApiClient(httpClientFactory, _bearerTokenProvider.Object, _logger.Object);
+
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await dut.QueryAndDeserializeAsync<DummyClass>(null));
+        }
+
+        [TestMethod]
+        public async Task QueryAndDeserialize_ThrowsException_WhenUrlTooLong()
+        {
+            var httpClientFactory = HttpHelper.GetHttpClientFactory(HttpStatusCode.OK, "");
+            var dut = new BearerTokenApiClient(httpClientFactory, _bearerTokenProvider.Object, _logger.Object);
+
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => await dut.QueryAndDeserializeAsync<DummyClass>(new string('u', 2001)));
         }
 
         private class DummyClass
         {
+            // ReSharper disable once UnusedAutoPropertyAccessor.Local
             public int Id { get; set; }
         }
     }
