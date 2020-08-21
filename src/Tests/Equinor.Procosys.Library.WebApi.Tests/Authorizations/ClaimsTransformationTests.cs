34010699653ae7b00427ab795c35b278e0d77454
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-08-21 10:06:50 +0200

    unit tests

--- src/Tests/Equinor.Procosys.Library.WebApi.Tests/Authorizations/ClaimsTransformationTests.cs
+++ src/Tests/Equinor.Procosys.Library.WebApi.Tests/Authorizations/ClaimsTransformationTests.cs
@@ -17,7 +17,10 @@ namespace Equinor.Procosys.Library.WebApi.Tests.Authorizations
         private ClaimsTransformation _dut;
         private Guid Oid = new Guid("{0B627D64-8113-40E1-9394-60282FB6BB9F}");
         private ClaimsPrincipal _principalWithOid;
-        private readonly string Plant = "P";
+        private readonly string Plant1 = "Plant1";
+        private readonly string Plant2 = "Plant2";
+        private readonly string Permission_Plant1 = Permissions.LIBRARY_GENERAL_READ;
+        private readonly string Permission_Plant2 = Permissions.LIBRARY_GENERAL_WRITE;
         private readonly string _tagReadPermission = "TAG/READ";
         private Mock<IPlantProvider> _plantProviderMock;
         private Mock<IPlantCache> _plantCacheMock;
@@ -26,14 +29,18 @@ namespace Equinor.Procosys.Library.WebApi.Tests.Authorizations
         public void Setup()
         {
             _plantProviderMock = new Mock<IPlantProvider>();
-            _plantProviderMock.SetupGet(p => p.Plant).Returns(Plant);
+            _plantProviderMock.SetupGet(p => p.Plant).Returns(Plant1);
 
             _plantCacheMock = new Mock<IPlantCache>();
-            _plantCacheMock.Setup(p => p.IsValidPlantForUserAsync(Plant, Oid)).Returns(Task.FromResult(true));
+            _plantCacheMock.Setup(p => p.IsValidPlantForUserAsync(Plant1, Oid)).Returns(Task.FromResult(true));
+            _plantCacheMock.Setup(p => p.IsValidPlantForUserAsync(Plant2, Oid)).Returns(Task.FromResult(true));
 
             var permissionCacheMock = new Mock<IPermissionCache>();
-            permissionCacheMock.Setup(p => p.GetPermissionsForUserAsync(Plant, Oid))
-                .Returns(Task.FromResult<IList<string>>(new List<string> {_tagReadPermission, Permissions.LIBRARY_GENERAL_READ, Permissions.LIBRARY_GENERAL_WRITE}));
+            permissionCacheMock.Setup(p => p.GetPermissionsForUserAsync(Plant1, Oid))
+                .Returns(Task.FromResult<IList<string>>(new List<string> {Permission_Plant1, _tagReadPermission}));
+
+            permissionCacheMock.Setup(p => p.GetPermissionsForUserAsync(Plant2, Oid))
+                .Returns(Task.FromResult<IList<string>>(new List<string> {Permission_Plant2, _tagReadPermission}));
 
             _principalWithOid = new ClaimsPrincipal();
             var claimsIdentity = new ClaimsIdentity();
@@ -48,10 +55,16 @@ namespace Equinor.Procosys.Library.WebApi.Tests.Authorizations
         {
             var result = await _dut.TransformAsync(_principalWithOid);
 
-            var roleClaims = GetRoleClaims(result.Claims);
-            Assert.AreEqual(2, roleClaims.Count);
-            Assert.IsTrue(roleClaims.Any(r => r.Value == Permissions.LIBRARY_GENERAL_READ));
-            Assert.IsTrue(roleClaims.Any(r => r.Value == Permissions.LIBRARY_GENERAL_WRITE));
+            AssertRoleClaimsForPlant1(result.Claims);
+        }
+        
+        [TestMethod]
+        public async Task TransformAsync_Twice_ShouldNotDuplicateRoleClaimsForPermissions()
+        {
+            await _dut.TransformAsync(_principalWithOid);
+            var result = await _dut.TransformAsync(_principalWithOid);
+
+            AssertRoleClaimsForPlant1(result.Claims);
         }
 
         [TestMethod]
@@ -60,7 +73,7 @@ namespace Equinor.Procosys.Library.WebApi.Tests.Authorizations
             var result = await _dut.TransformAsync(_principalWithOid);
 
             var roleClaims = GetRoleClaims(result.Claims);
-            Assert.AreEqual(2, roleClaims.Count);
+            Assert.AreEqual(1, roleClaims.Count);
             Assert.IsFalse(roleClaims.Any(r => r.Value == _tagReadPermission));
         }
 
@@ -81,8 +94,29 @@ namespace Equinor.Procosys.Library.WebApi.Tests.Authorizations
 
             Assert.AreEqual(0, GetRoleClaims(result.Claims).Count);
         }
+                
+        [TestMethod]
+        public async Task TransformAsync_OnSecondPlant_ShouldClearAllClaimsForFirstPlant()
+        {
+            var result = await _dut.TransformAsync(_principalWithOid);
+            AssertRoleClaimsForPlant1(result.Claims);
+
+            _plantProviderMock.SetupGet(p => p.Plant).Returns(Plant2);
+            result = await _dut.TransformAsync(_principalWithOid);
+
+            var claims = GetRoleClaims(result.Claims);
+            Assert.AreEqual(1, claims.Count);
+            Assert.IsNotNull(claims.SingleOrDefault(r => r.Value == Permission_Plant2));
+        }
 
         private static List<Claim> GetRoleClaims(IEnumerable<Claim> claims)
             => claims.Where(c => c.Type == ClaimTypes.Role).ToList();
+
+        private void AssertRoleClaimsForPlant1(IEnumerable<Claim> claims)
+        {
+            var roleClaims = GetRoleClaims(claims);
+            Assert.AreEqual(1, roleClaims.Count);
+            Assert.IsNotNull(roleClaims.SingleOrDefault(r => r.Value == Permission_Plant1));
+        }
     }
 }
