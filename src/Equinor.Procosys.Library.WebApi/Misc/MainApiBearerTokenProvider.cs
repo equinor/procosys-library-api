ae0d867bda48cd402041507e11b0a71bca347a71
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2022-10-11 08:39:52 +0200

    Log first part of secret (#252)

--- src/Equinor.Procosys.Library.WebApi/Misc/MainApiBearerTokenProvider.cs
+++ src/Equinor.Procosys.Library.WebApi/Misc/MainApiBearerTokenProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Equinor.Procosys.Library.Query.Client;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
@@ -14,11 +15,14 @@ namespace Equinor.Procosys.Library.WebApi.Misc
         private readonly string _clientId;
         private readonly string _clientSecret;
         private readonly string _mainApiAudience;
+        private readonly ILogger<MainApiBearerTokenProvider> _logger;
+        private readonly string _secretInfo;
 
         public MainApiBearerTokenProvider(
             IHttpContextAccessor httpContextAccessor,
             IOptionsMonitor<ApiOptions> apiOptions,
-            IOptionsMonitor<MainApiOptions> mainApiOptions)
+            IOptionsMonitor<MainApiOptions> mainApiOptions,
+            ILogger<MainApiBearerTokenProvider> logger)
         {
             _httpContextAccessor = httpContextAccessor;
 
@@ -27,10 +31,13 @@ namespace Equinor.Procosys.Library.WebApi.Misc
             _clientSecret = apiOptions.CurrentValue.ClientSecret;
 
             _mainApiAudience = mainApiOptions.CurrentValue.Audience;
+            _secretInfo = $"{_clientSecret.Substring(0, 2)}***{_clientSecret.Substring(_clientSecret.Length - 1, 1)}";
+            _logger = logger;
         }
 
         public async Task<string> GetBearerTokenAsync()
         {
+            _logger.LogInformation($"Getting client credentials using {_secretInfo} for {_clientId}");
             var clientCred = new ClientCredential(_clientId, _clientSecret);
 
             var authorizationHeader = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
