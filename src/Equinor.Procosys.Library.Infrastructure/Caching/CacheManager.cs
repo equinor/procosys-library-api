3e03411ed7f16192e789a22389248cce7c022395
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2022-11-08 08:31:11 +0100

    96114 improve cache (#256)
    * Made CacheManager testable (as IPO)
    
    * Improve CacheManager: if cached value is a task, check if that task was Successfully executed before returning cached value

--- src/Equinor.Procosys.Library.Infrastructure/Caching/CacheManager.cs
+++ src/Equinor.Procosys.Library.Infrastructure/Caching/CacheManager.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Equinor.Procosys.Library.Domain.Time;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Internal;
 
 namespace Equinor.Procosys.Library.Infrastructure.Caching
 {
@@ -7,7 +10,10 @@ namespace Equinor.Procosys.Library.Infrastructure.Caching
     {
         private readonly IMemoryCache _cache;
 
-        public CacheManager() => _cache = new MemoryCache(new MemoryCacheOptions());
+        public CacheManager() => _cache = new MemoryCache(new MemoryCacheOptions
+        {
+            Clock = new CacheClock()
+        });
 
         public T Get<T>(string key) where T : class => _cache.Get(key) as T;
 
@@ -18,7 +24,13 @@ namespace Equinor.Procosys.Library.Infrastructure.Caching
             var instance = Get<T>(key);
             if (instance != null)
             {
-                return instance;
+                // return value from cache only if cached value is not a Task, or if the Task (i.e the async operation) has completed Successfully
+                // what could happen before rewrite, was when calling an endpoint such as getting Plants, and for some reason that endpoint failed,
+                // the cache continue to return the failed Task until cache expired
+                if (instance is not Task t || t.IsCompletedSuccessfully)
+                {
+                    return instance;
+                }
             }
 
             instance = fetch.Invoke();
@@ -33,7 +45,7 @@ namespace Equinor.Procosys.Library.Infrastructure.Caching
                 return;
             }
 
-            _cache.Set(key, instance, DateTime.UtcNow.Add(GetExpirationTime(duration, expiration)));
+            _cache.Set(key, instance, TimeService.UtcNow.Add(GetExpirationTime(duration, expiration)));
         }
 
         private static TimeSpan GetExpirationTime(CacheDuration duration, long expiration)
@@ -44,5 +56,10 @@ namespace Equinor.Procosys.Library.Infrastructure.Caching
                 CacheDuration.Seconds => TimeSpan.FromSeconds(expiration),
                 _ => throw new NotImplementedException($"Unknown {nameof(CacheDuration)}: {duration}"),
             };
+
+        private class CacheClock : ISystemClock
+        {
+            public DateTimeOffset UtcNow => TimeService.UtcNow;
+        }
     }
 }
