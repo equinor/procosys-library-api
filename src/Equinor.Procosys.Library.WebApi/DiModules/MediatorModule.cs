b873ab46dc9d1f5b2fc56be1983d3574ed0fa414
Christian Hormazabal <78788930+chormaz@users.noreply.github.com>
2023-03-21 14:52:50 +0100

    Tech/96324 nuget updates (#272)
    * nuget updates
    
    * Necessary changes in MediatorModule

--- src/Equinor.Procosys.Library.WebApi/DiModules/MediatorModule.cs
+++ src/Equinor.Procosys.Library.WebApi/DiModules/MediatorModule.cs
@@ -11,11 +11,16 @@ namespace Equinor.Procosys.Library.WebApi.DIModules
     {
         public static void AddMediatrModules(this IServiceCollection services)
         {
-            services.AddMediatR(
-                typeof(MediatorModule).GetTypeInfo().Assembly,
-                typeof(ICommandMarker).GetTypeInfo().Assembly,
-                typeof(IQueryMarker).GetTypeInfo().Assembly
-            );
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(
+                    typeof(MediatorModule).GetTypeInfo().Assembly,
+                    typeof(ICommandMarker).GetTypeInfo().Assembly,
+                    typeof(IQueryMarker).GetTypeInfo().Assembly
+                );
+
+            });
+
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
         }
